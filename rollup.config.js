876f8318562382dc59e53689a2a4b48d7eacaac2
Jimmie <lee2py@gmail.com>
2021-01-05 15:27:11 +0800

    config rollup for typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import pkg from './package.json';
-import babel from 'rollup-plugin-babel';
+// import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 let config = {
   input: 'src/handy-scroll.ts',
@@ -23,21 +24,40 @@ ${pkg.homepage}
 export default [
   {
     input: config.input,
-    output: Object.assign({file: 'dist/handy-scroll.es6.js'}, config.output),
+    output: {
+      ...config.output,
+      file: 'dist/handy-scroll.es.js',
+      format: 'es',
+    },
+    plugins: [typescript({tsconfigOverride: {compilerOptions: {target: 'ES6'}}})]
   },
   {
     input: config.input,
-    output: Object.assign({file: 'dist/handy-scroll.es6.min.js'}, config.output),
-    plugins: config.plugins
+    output: {...config.output, file: 'dist/handy-scroll.es.min.js', format: 'es'},
+    plugins: [typescript({tsconfigOverride: {compilerOptions: {target: 'ES6'}}}), ...config.plugins]
   },
   {
     input: config.input,
-    output: Object.assign({file: 'dist/handy-scroll.js'}, config.output),
-    plugins: [babel()]
+    output: {
+      ...config.output,
+      file: 'dist/handy-scroll.js',
+      format: 'es',
+    },
+    plugins: [typescript()]
   },
   {
     input: config.input,
-    output: Object.assign({file: 'dist/handy-scroll.min.js'}, config.output),
-    plugins: [babel()].concat(config.plugins)
+    output: {...config.output, file: 'dist/handy-scroll.min.js', format: 'es'},
+    plugins: [typescript(), ...config.plugins]
+  },
+  {
+    input: config.input,
+    output: Object.assign({file: 'dist/handy-scroll.umd.js'}, config.output),
+    plugins: [typescript()]
+  },
+  {
+    input: config.input,
+    output: Object.assign({file: 'dist/handy-scroll.umd.min.js'}, config.output),
+    plugins: [typescript(), ...config.plugins]
   }
 ];
